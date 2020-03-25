1f29db2f529d56fcd9b4525b072917d9ecf49983
Storm King <sking78@uncc.edu>
2020-03-24 20:00:38 -0400

    Created JobType Model, Repository, Controller, Exception, and added defaults to DataLoader

--- src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/bootstrap/DataLoader.java
+++ src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/bootstrap/DataLoader.java
@@ -5,7 +5,9 @@
  */
 package com.SSDIproject.ManpowerAllocatorSSDI.bootstrap;
 
+import com.SSDIproject.ManpowerAllocatorSSDI.model.JobTypes;
 import com.SSDIproject.ManpowerAllocatorSSDI.model.Student;
+import com.SSDIproject.ManpowerAllocatorSSDI.repository.JobTypesRepository;
 import com.SSDIproject.ManpowerAllocatorSSDI.repository.StudentRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -15,10 +17,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    private final StudentRepository repository;
+    private final StudentRepository sRepository;
+    private final JobTypesRepository jtRepository;
 
-    public DataLoader(StudentRepository repository) {
-        this.repository = repository;
+    public DataLoader(StudentRepository sRepository, JobTypesRepository jtRepository) {
+        this.sRepository = sRepository;
+        this.jtRepository = jtRepository;
     }
 
     @Override
@@ -27,18 +31,30 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         student_a.setFirstName("Jane");
         student_a.setLastName("Doe");
         student_a.setYear("Junior");
-        repository.save(student_a);
+        sRepository.save(student_a);
 
         Student student_b = new Student();
         student_b.setFirstName("Martin");
         student_b.setLastName("Fowler");
         student_b.setYear("Senior");
-        repository.save(student_b);
+        sRepository.save(student_b);
 
         Student student_c = new Student();
         student_c.setFirstName("Roy");
         student_c.setLastName("Fielding");
         student_c.setYear("Freshman");
-        repository.save(student_c);
+        sRepository.save(student_c);
+        
+        JobTypes jobType_a = new JobTypes();
+        jobType_a.setTypeName("Test Type 1");
+        jtRepository.save(jobType_a);
+        
+        JobTypes jobType_b = new JobTypes();
+        jobType_a.setTypeName("Test Type 2");
+        jtRepository.save(jobType_b);
+        
+        JobTypes jobType_c = new JobTypes();
+        jobType_a.setTypeName("Test Type 3");
+        jtRepository.save(jobType_c);
     }
 }
