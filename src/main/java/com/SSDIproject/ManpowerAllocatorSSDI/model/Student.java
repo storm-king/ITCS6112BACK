7941c05087352d5f1a632203a16f212c8ed4cd8a
Storm King <sking78@uncc.edu>
2020-03-25 15:43:41 -0400

    Created JobType Model, Repository, Controller, Exception, and added defaults to DataLoader

--- src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/model/Student.java
+++ src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/model/Student.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 public class Student {
  
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
      
     private String firstName;
