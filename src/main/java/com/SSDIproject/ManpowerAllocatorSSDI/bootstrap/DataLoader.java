b7346515ad71d101c8b768f3f0472a886be3f1a5
Storm King <sking78@uncc.edu>
2020-03-25 15:42:52 -0400

    Created JobType Model, Repository, Controller, Exception, and added defaults to DataLoader

--- src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/bootstrap/DataLoader.java
+++ src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/bootstrap/DataLoader.java
@@ -6,9 +6,7 @@
 package com.SSDIproject.ManpowerAllocatorSSDI.bootstrap;
 
 import com.SSDIproject.ManpowerAllocatorSSDI.model.JobTypes;
-import com.SSDIproject.ManpowerAllocatorSSDI.model.Student;
 import com.SSDIproject.ManpowerAllocatorSSDI.repository.JobTypesRepository;
-import com.SSDIproject.ManpowerAllocatorSSDI.repository.StudentRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -17,44 +15,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    private final StudentRepository sRepository;
     private final JobTypesRepository jtRepository;
 
-    public DataLoader(StudentRepository sRepository, JobTypesRepository jtRepository) {
-        this.sRepository = sRepository;
+    public DataLoader(JobTypesRepository jtRepository) {
         this.jtRepository = jtRepository;
     }
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        Student student_a = new Student();
-        student_a.setFirstName("Jane");
-        student_a.setLastName("Doe");
-        student_a.setYear("Junior");
-        sRepository.save(student_a);
-
-        Student student_b = new Student();
-        student_b.setFirstName("Martin");
-        student_b.setLastName("Fowler");
-        student_b.setYear("Senior");
-        sRepository.save(student_b);
-
-        Student student_c = new Student();
-        student_c.setFirstName("Roy");
-        student_c.setLastName("Fielding");
-        student_c.setYear("Freshman");
-        sRepository.save(student_c);
-        
-        JobTypes jobType_a = new JobTypes();
-        jobType_a.setTypeName("Test Type 1");
-        jtRepository.save(jobType_a);
-        
-        JobTypes jobType_b = new JobTypes();
-        jobType_a.setTypeName("Test Type 2");
-        jtRepository.save(jobType_b);
-        
-        JobTypes jobType_c = new JobTypes();
-        jobType_a.setTypeName("Test Type 3");
-        jtRepository.save(jobType_c);
+    public void onApplicationEvent(ContextRefreshedEvent event) {    
+//   EXAMPLE OF HOW TO LOAD DEFAULT DATA TO DATABASE ON APP START:
+//        JobTypes jobType_a = new JobTypes();
+//        jobType_a.setTypeName("Test Type 1");
+//        jtRepository.save(jobType_a);
     }
 }
