589a0a7e7a1ff06f7001bb05c658d40e39582078
Storm King <sking78@uncc.edu>
2020-03-25 15:42:24 -0400

    Created JobType Model, Repository, Controller, Exception, and added defaults to DataLoader

---
+++ src/main/java/com/SSDIproject/ManpowerAllocatorSSDI/repository/JobTypesRepository.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.SSDIproject.ManpowerAllocatorSSDI.repository;
+
+import com.SSDIproject.ManpowerAllocatorSSDI.model.JobTypes;
+import org.springframework.data.repository.CrudRepository;
+ 
+public interface JobTypesRepository extends CrudRepository<JobTypes, Integer> {
+ 
+}
